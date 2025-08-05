a1fd294cad25fa1ce01d4bcc0d9a5150414b43fe
Simone Pizzamiglio <me@simonepizzamiglio.com>
2025-08-05 21:38:47 +0200

    fix prerender paths

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,11 @@
 import type { Config } from '@react-router/dev/config';
 
+const ssrOnlyPaths = ['/resume'];
+
 export default {
   ssr: true,
-  prerender: true,
+  prerender: ({ getStaticPaths }) => {
+    const paths = getStaticPaths();
+    return paths.filter((path) => !ssrOnlyPaths.includes(path));
+  },
 } satisfies Config;
