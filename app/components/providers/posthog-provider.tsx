4580aadbd3639c915c09432fd2a1043a43864897
Simone Pizzamiglio <me@simonepizzamiglio.com>
2025-08-06 15:32:27 +0200

    capture posthog event

--- app/components/providers/posthog-provider.tsx
+++ app/components/providers/posthog-provider.tsx
@@ -2,30 +2,22 @@ import { useEffect, useState } from 'react';
 import posthog from 'posthog-js';
 import { PostHogProvider } from 'posthog-js/react';
 
-function PHProviderInternal({ children }: { children: React.ReactNode }) {
+export function PHProvider({ children }: { children: React.ReactNode }) {
   const [hydrated, setHydrated] = useState(false);
 
   useEffect(() => {
-    posthog.init(process.env.POSTHOG_KEY!, {
-      api_host: 'https://eu.i.posthog.com',
-      defaults: '2025-05-24',
-      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-    });
+    if (process.env.POSTHOG_KEY) {
+      posthog.init(process.env.POSTHOG_KEY, {
+        api_host: 'https://eu.i.posthog.com',
+        defaults: '2025-05-24',
+        person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+      });
+      posthog.capture('posthog-init', { property: 'post-init' });
+    }
 
     setHydrated(true);
   }, []);
 
   if (!hydrated) return <>{children}</>;
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
-}
-
-export function PHProvider({ children }: { children: React.ReactNode }) {
-  if (
-    process.env.NODE_ENV === 'development' ||
-    process.env.NODE_ENV === 'test' ||
-    process.env.POSTHOG_KEY === undefined
-  ) {
-    return <>{children}</>;
-  }
-  return <PHProviderInternal>{children}</PHProviderInternal>;
 }
