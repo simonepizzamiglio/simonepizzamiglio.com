5c7ce7903927805f648d0c57da85cb0523f1a669
Simone Pizzamiglio <me@simonepizzamiglio.com>
2025-08-06 09:57:50 +0200

    test: remove prerender attribute

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,11 +1,13 @@
 import type { Config } from '@react-router/dev/config';
+import { vercelPreset } from '@vercel/react-router/vite';
 
-const ssrOnlyPaths = ['/resume'];
+// const ssrOnlyPaths = ['/resume'];
 
 export default {
   ssr: true,
-  prerender: ({ getStaticPaths }) => {
-    const paths = getStaticPaths();
-    return paths.filter((path) => !ssrOnlyPaths.includes(path));
-  },
+  // prerender: ({ getStaticPaths }) => {
+  //   const paths = getStaticPaths();
+  //   return paths.filter((path) => !ssrOnlyPaths.includes(path));
+  // },
+  presets: [vercelPreset()],
 } satisfies Config;
