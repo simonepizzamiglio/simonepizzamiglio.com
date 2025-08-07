1903c016ed39b2c1b10bfa350694c286d3cc9295
Simone Pizzamiglio <me@simonepizzamiglio.com>
2025-08-07 10:33:31 +0200

    do not prerender /action/set-theme

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,13 +1,13 @@
 import type { Config } from '@react-router/dev/config';
 import { vercelPreset } from '@vercel/react-router/vite';
 
-const ssrOnlyPaths = ['/resume'];
+const notPrerenderPaths = ['/resume', '/action/set-theme'];
 
 export default {
   ssr: true,
   prerender: ({ getStaticPaths }) => {
     const paths = getStaticPaths();
-    return paths.filter((path) => !ssrOnlyPaths.includes(path));
+    return paths.filter((path) => !notPrerenderPaths.includes(path));
   },
   presets: [vercelPreset()],
 } satisfies Config;
