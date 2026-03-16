53127a9e194e6512bffda2d1ef2cd11dc2697ad5
Simone Pizzamiglio <me@simonepizzamiglio.com>
2026-03-16 08:13:31 +0100

    switch to bun and add Cloudfare workers config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,21 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 import { URL, fileURLToPath } from "node:url"
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://simonepizzamiglio.com",
-	integrations: [mdx(), sitemap()],
+  site: "https://simonepizzamiglio.com",
+  integrations: [mdx(), sitemap()],
 
-	vite: {
-		plugins: [tailwindcss()],
-		resolve: {
-			alias: {
-				"#": fileURLToPath(new URL("./src", import.meta.url)),
-			},
-		},
+  vite: {
+      plugins: [tailwindcss()],
+      resolve: {
+          alias: {
+              "#": fileURLToPath(new URL("./src", import.meta.url)),
+          },
+      },
 	},
+
+  adapter: cloudflare(),
 })
